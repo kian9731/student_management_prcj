cb142205c5064472fde045e2f7b7275bd898ec50
k <kian70807090@gmail.com>
2023-06-27 12:34:17 +0330

    change student screen

--- lib/screens/student_page.dart
+++ lib/screens/student_page.dart
@@ -8,54 +8,12 @@ class StudentPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Students List"),
+        title: Text("Student List"),
       ),
-      body: Column(
-        children: [
-          SizedBox(
-            width: 160,
-            height: 50,
-            child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    primary: Colors.orange,
-                    backgroundColor: Colors.orange
-                ),
-                onPressed: (){
-                  Navigator.pushNamed(context, "/createStudent");
-                },
-                child:const Text("Create a Student",style: TextStyle(
-                  color: Colors.white , fontSize: 17,
-                ),)),
-          ),
-          FutureBuilder(
-            future: getStudents(),
-            builder: (context, snapshot){
-              if(snapshot.hasData){
-                return Expanded(
-                  child: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: List.generate(
-                        20,
-                            (index) => createItem(context, snapshot.data![index].firstName),
-                      ),
-                    ),
-                  ),
-                );
-              }
-              else if(snapshot.hasError){
-                return Center(child:Text(snapshot.error.toString()));
-              }
-              else{
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-            },
-          ),
-        ],
-      ),
+      body:Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [],
+      )
     );
   }
 
